0d2a91573585bc77a1cfc4a95287344747f52307
sepante <sinasajjadi@ymail.com>
2017-10-23 15:20:37 +0330

    starting...

--- main.cpp
+++ main.cpp
@@ -1,45 +1,116 @@
-// exponential_distribution
 #include <iostream>
-#include <fstream>
 #include <random>
+#include <fstream>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/graph_utility.hpp>
+#include <boost/graph/properties.hpp>
+#include <boost/config.hpp>
+#include "SIR.h"
+#include "global.h"
+using namespace boost;
+
+//typedef adjacency_list<listS, vecS, undirectedS, SIR, Interaction> Network;
+//typedef adjacency_list<listS, vecS, undirectedS, SIR> Network;
+typedef adjacency_list<vecS, vecS, undirectedS, SIR> Network;
+typedef graph_traits<Network>::edges_size_type Edge_Num;
+typedef graph_traits<Network>::vertices_size_type Vertex_Num;
+typedef graph_traits<Network>::vertex_iterator Vertex_iter;
+typedef graph_traits<Network>::vertex_descriptor Vertex;
+typedef graph_traits<Network>::edge_descriptor Edge;
+typedef graph_traits<Network>::edge_iterator Edge_iter;
+
+
+
 /*
+class Interaction
+{
+	public:
+	int present = 1;
+};
+*/
+
+//typedef adjacency_list<listS, vecS, undirectedS, SIR, Interaction> Network;
+Vertex_Num vert_num = 128;
+//Vertex_Num vert_num = 10;
+Network society(vert_num);
+
+float cnct_prob = (float)4/(float)vert_num;
+float r, p = 0.25, q = 1;
+int duration = 1000;
+double t;
+//std::set <int> actives={};
+std::vector<std::vector<int> > I;
+std::vector<std::vector<int> > R;
+
+
+void init_states()
+{
+	for( Vertex vd : make_iterator_range( vertices(society) ) )
+	{
+		society[vd].health = 1;
+		society[vd].future = 1;
+	}
+	int seed = rand() % vert_num;
+	//int seed = 2;
+	Vertex v = vertex(seed, society);
+	society[v].health = 6;
+	society[v].future = 6;
+	//society_origin = Network(society);
+	actives = {};
+	actives.insert(seed);
+}
+
 int main()
 {
+  clock_t begin = clock();
   //std::default_random_engine generator(std::random_device{}());
-  int a = 2;
-  std::default_random_engine generator(1);
-  std::exponential_distribution<double> distribution(a);
+  std::default_random_engine generator;
   std::ofstream fout;
-  fout.open("cdata.txt");
-  //a= 10;
+	fout.open("cdata.txt");
+  double dt;
+  std::exponential_distribution<double> exp(1);
+	//Network society_origin;
+	float cnct_prob = (float)4/(float)vert_num;
 
-  for (int i=0; i<1000; i++)
+	 // Erdos Renyi constructor.
+		for (size_t i = 0; i < vert_num; i++)
+		{
+			for (size_t j = i+1; j < vert_num; j++)
+			{
+				if ( dice(cnct_prob) )
+				{
+					add_edge(i, j, society);
+					//std::cout << i << "-->" << j << '\n';
+				}
+			}
+		}
+
+
+  for (size_t i = 0; t<= duration; i++)
   {
-    double number = distribution(generator);
-    fout<<number<<'\n';
-    std::cerr << number << '\n';
+    dt = exp(generator);
+    fout<<dt<<'\n';
+		t += dt;
   }
 
-}
-*/
-// exponential_distribution::param
-#include <iostream>
-#include <random>
+	SIR patient;
+	//patient.health *= 2;
+	//int jafar = a;
+	//std::cout << jafar << '\n';
 
-int main()
-{
-  //std::default_random_engine generator(std::random_device{}());
-  std::default_random_engine generator();
-  std::exponential_distribution<double> d1(1);
-  std::exponential_distribution<double> d2(d1.param());
-  auto ptr = &d1;
-  std::cout << *ptr << '\n';
-  //std::exponential_distribution<double>::param_type new_lambda(0.2);
+  std::cout << "END" << '\n';
+  clock_t end = clock();
+  double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+	//std::cout << "time: " << elapsed_secs << '\n';
+	std::cout << "t: " << t << '\n';
+	boost::print_graph(society);
+
+	//for ( Vertex vi : make_iterator_range( adjacent_vertices(0, society) ) )
+	for ( Edge vi : make_iterator_range( in_edges(0, society) ) )
+	{
+		//society[vi].turn_I( society[vd].supply() );
+		std::cout << vi << '\n';
+	}
 
 
-  // print two independent values:
-  std::cout << d1(generator) << std::endl;
-  std::cout << d2(generator) << std::endl;
-  //std::exponential_distribution<double>
-  return 0;
 }
