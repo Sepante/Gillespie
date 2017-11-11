e58d4e1e00138aeff01b461dac4b5a2a7c23adaa
sepante <sinasajjadi@ymail.com>
2017-11-11 03:35:52 +0330

    not sure..

--- plotCdata.py
+++ plotCdata.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 
 
+with open('16384_onedis.txt') as f:
 #with open('cdata.txt') as f:
-with open('cdata.txt') as f:
     data=[float(i) for i in f]
 
 n_size = int(data.pop(0))
@@ -37,12 +37,13 @@ for qindex in range(q_size):
     #plt.suptitle("$2DGrid$ $ q= %.1f$, $N= %d$"%(q,n))
     #plt.suptitle("$Erdos$ $ q= %.1f$, $N= %d$"%(q,n))
     #plt.suptitle("$Phase: $"+" $n=$"+str(n)+ ", $q=$" + str(qrange[qindex]) + ", $r=$" + str(rrange[0]))
-    name_string = "$Gillespie$ $Erdos$" +"$Phase: $"+" $n=$"+str(n)+ ", $q=$" + str(qrange[qindex]) + ", $r=$" + str(rrange[0])
+    name_string = " $one$ $disease$ $Gillespie$ $Erdos$" +"$Phase: $"+" $n=$"+str(n)+ ", $q=$" + str(qrange[qindex]) + ", $r=$" + str(rrange[0])
     plt.suptitle(name_string)
     plt.xlabel('$p$')
     plt.ylabel('$R$')
     plt.ylim([0-0.02,1+0.02])
-    #plt.savefig("Phase: "+"n="+str(n)+ ", q=" + str(qrange[qindex]) + ", r=" + str(rrange[0]) + ".png")
+    name_string = name_string.replace('$','')
+    
     location = "results/"
     plt.savefig(location+name_string+".png")
     plt.show()
